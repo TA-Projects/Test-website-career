ae1ea15005a21176694a0f074fbe2f80d6c8f1b8
TA-Projects <asmonastautvydas@gmail.com>
2024-07-15 09:00:24 +0000

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -34,6 +34,10 @@ job_list = [
 @app.route('/')
 def hello_world():
     return render_template('home.html', jobs=job_list)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(job_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
